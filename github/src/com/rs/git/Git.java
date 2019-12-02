66b75c64f34df9b671156f2b3f7b52125247bf2e
DELL <DELL@DESKTOP-EJ38E7A>
2019-12-02 18:02:21 +0530

    pravalika

--- github/src/com/rs/git/Git.java
+++ github/src/com/rs/git/Git.java
@@ -10,5 +10,11 @@ public class Git {
 	public void setId(int id) {
 		this.id = id;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
